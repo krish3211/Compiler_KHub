3d184e4164cd70e6d00c6e6fcb5f2fe568a13d15
Dune Krishna Sai Manikanta Dora <krishnapavan3211@gmail.com>
2023-08-22 14:41:31 +0530

    completed submit code

--- Server/routes/SQuestion.js
+++ Server/routes/SQuestion.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fetchuser = require("../middleware/fetchauth");
+const PQuestion = require("../models/ProblemQue");
 const SQuestion = require("../models/SubmitQ");
 
 router.post("/addSubmit/:id", fetchuser, async (req, res) => {
@@ -8,7 +9,53 @@ router.post("/addSubmit/:id", fetchuser, async (req, res) => {
     // let note = await SQuestion.findById(req.params.id);
     const { passedtestcases, actualtestcases, testpercentage, codes } = req.body;
     let submit = await SQuestion.find({ user: req.user.id }).select("pquestion");
-    if (submit[0]["pquestion"].toString() === req.params.id) {
+    const targetId = req.params.id;
+
+    const conductonArray = submit.map((item) => item["pquestion"].toString());
+    const isConductionTrue = conductonArray.includes(targetId);
+    if (isConductionTrue) {
+      return res.status(500).send("Already data exist");
+    }
+    const sques = new SQuestion({
+      user: req.user.id,
+      pquestion: req.params.id,
+      passedtestcases,
+      actualtestcases,
+      testpercentage,
+      codes,
+    });
+
+    const savedS = await sques.save();
+    res.json(savedS);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/fetching", fetchuser, async (req, res) => {
+  try {
+    let sumit = await SQuestion.find({ user: req.user.id });
+
+    const pquestionIds = sumit.map((item) => item["pquestion"]);
+    let fetch = await PQuestion.find({ _id: { $in: pquestionIds } });
+    res.json({ sumit, fetch });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/resubmit/:id", fetchuser, async (req, res) => {
+  try {
+    // let note = await SQuestion.findById(req.params.id);
+    const { passedtestcases, actualtestcases, testpercentage, codes } = req.body;
+    let submit = await SQuestion.find({ user: req.user.id }).select("pquestion");
+    const targetId = req.params.id;
+
+    const conductonArray = submit.map((item) => item["pquestion"].toString());
+    const isConductionTrue = conductonArray.includes(targetId);
+    if (isConductionTrue) {
       const newNote = {};
       if (passedtestcases) {
         newNote.passedtestcases = passedtestcases;
@@ -22,35 +69,16 @@ router.post("/addSubmit/:id", fetchuser, async (req, res) => {
       if (codes) {
         newNote.codes = codes;
       }
-      let sques = await SQuestion.findById(submit[0]["_id"]);
-    if (!sques) {
-      return res.status(404).send("Not Found");
-    }
-      sques = await SQuestion.findByIdAndUpdate(submit[0]["_id"], { $set: newNote }, { new: true });
-      
+      let sques = await SQuestion.find({pquestion:req.params.id});
+      if (!sques) {
+        return res.status(404).send("Not Found");
+      }
+      sques = await SQuestion.findOneAndUpdate({pquestion:req.params.id}, { $set: newNote }, { new: true });
+
       return res.json({ sques });
     }
-    const sques = new SQuestion({
-      user: req.user.id,
-      pquestion: req.params.id,
-      passedtestcases,
-      actualtestcases,
-      testpercentage,
-      codes,
-    });
 
-    const savedS = await sques.save();
-    res.json("savedS");
-  } catch (error) {
-    console.error(error.message);
     res.status(500).send("Internal Server Error");
-  }
-});
-
-router.post("/fetching", fetchuser, async (req, res) => {
-  try {
-    let sumit = await SQuestion.find({ user: req.user.id }).select(["pquestion"]);
-    res.json(sumit);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Server Error");
